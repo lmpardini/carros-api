b1e3ea806874a61e05fa64dbf8efd8307a1cf7c9
Lucas Pardini <lucas.pardini@dcide.com.br>
2022-07-08 13:56:07 -0300

    Implementação Rotas VeiculosController

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/** Rotas VeiculosController */
+
+Route::prefix('veiculos')->controller(\App\Http\Controllers\VeiculosController::class)->group(function () {
+    Route::get('/', 'listarVeiculos');
+    Route::get('/find', 'buscarVeiculo');
+    Route::get('/{id}', 'listarVeiculosPorId');
+    Route::post('/', 'criarVeiculo');
+    Route::put('/{id}', 'editarVeiculoPorId');
+    Route::delete('/{id}', 'excluirVeiculoPorId');
+
 });
